b8d7098973b566988c63536d870ed8dabee04d66
parasol-ka <ananastya.chau@gmail.com>
2024-12-28 09:47:19 +0100

    create reprository

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Rohan</title>
+</head>
+<body>
+    <header>
+
+    </header>
+    <div id="renforts">
+        <img src="" alt="">
+        <div id="renforts_liste">
+
+        </div>
+    </div>
+    <footer id="comandants">
+
+    </footer>
+    
+</body>
+</html>
