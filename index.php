4ca6256bf2ce2cfcfee996d5c961b8636185cacc
parasol-ka <ananastya.chau@gmail.com>
2024-12-28 11:40:05 +0100

    list renforts ok

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
+<?php
+    include "_connexionBD.php";
+
+    $reqRenforts=$bd->prepare("SELECT * FROM renforts ORDER BY cavaliers DESC");
+    $reqRenforts->execute();
+
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -18,8 +26,56 @@
     <div id="renforts">
         <img src="rohirrims.jpg" alt="group of soldiers near a custletown in war">
         <div id="renforts_liste">
-            <p>lalal</p>
-            <p>lala</p>
+
+            <?php
+                while($renforts=$reqRenforts->fetch()){
+                    $id_renfort=$renforts['id_renfort'];
+                    $clan_name=$renforts['clan'];
+                    $cavaliers=$renforts['cavaliers'];
+                    $border_color=$renforts['couleur_banniere'];
+                    $id_commandant=$renforts['id_commandant'];
+                    $estimation=$renforts['estimation'];
+
+                    if ($cavaliers>=1000){
+                        $font_size='28px';
+                    }elseif ($cavaliers>=100 and $cavaliers<=999){
+                        $font_size='24px';
+                    }elseif ($cavaliers>=10 and $cavaliers<=99){
+                        $font_size='20px';
+                    }elseif ($cavaliers>=0 and $cavaliers<=9){
+                        $font_size='16px';
+                    }else {$font_size='14px';}
+
+                    echo "<a href='upload.php?page=$id_renfort' class='renforts_links' style='font-size: $font_size; border-left: $border_color solid 10px;'>$clan_name : $cavaliers";
+                    
+                    echo "<span id='img_container'>";
+
+                    if ($estimation > 0) {
+                        $pourcentage = ($cavaliers * 100) / $estimation;
+
+                        if ($pourcentage < 25) {
+                            echo "<img src='exclamation.png' alt='attention' class='exclamations'>
+                                <img src='exclamation.png' alt='attention' class='exclamations'>
+                                <img src='exclamation.png' alt='attention' class='exclamations'>";
+                        } elseif ($pourcentage >= 25 and $pourcentage < 50) {
+                            echo "<img src='exclamation.png' alt='attention' class='exclamations'>
+                                <img src='exclamation.png' alt='attention' class='exclamations'>";
+                        } elseif ($pourcentage >= 50 and $pourcentage < 75) {
+                            echo "<img src='exclamation.png' alt='attention' class='exclamations'>";
+                        } else {echo "";}
+                    } elseif ($estimation=0) {
+                        echo "<img src='exclamation.png' alt='attention' class='exclamations'>
+                                <img src='exclamation.png' alt='attention' class='exclamations'>
+                                <img src='exclamation.png' alt='attention' class='exclamations'>";
+                    }else {echo "";}
+
+                    echo "</span>";
+                    
+                    echo "</a><br>";
+                }
+            
+            ?>    
+
         </div>
     </div>
     <footer id="comandants">
