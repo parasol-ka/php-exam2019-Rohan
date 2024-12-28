50e75cf54b7d2fd2d7d98990ec10d4b3354b95f5
parasol-ka <ananastya.chau@gmail.com>
2024-12-28 10:11:06 +0100

    basic sctructure html

--- index.php
+++ index.php
@@ -8,16 +8,23 @@
 </head>
 <body>
     <header>
-
+        <h1>Le Rassemblement des Rohirrims À Dunharrow</h1>
+        <p>Le roi Théoden du Rohan mobilise ses troupes à Dunharrow pour partir à la guerre, défendre le Gondor.
+            Rapidement, les renforts venus des 4 coins du pays se joignent à la grande armée du roi, qui a bien du 
+            mal à compter le nombre de ses cavaliers. <br>
+            Heureusement, il peut compter sur vous pour coder une interface en PHP reliée à une base de données 
+            MySQL pour mettre à jour le nombre de cavaliers se joignant à l'armée. </p>
     </header>
     <div id="renforts">
-        <img src="" alt="">
+        <img src="rohirrims.jpg" alt="group of soldiers near a custletown in war">
         <div id="renforts_liste">
-
+            <p>lalal</p>
+            <p>lala</p>
         </div>
     </div>
     <footer id="comandants">
-
+        <p>lala</p>
+        
     </footer>
     
 </body>
